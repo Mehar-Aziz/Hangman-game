cdb025456fbdfcbf755cb89b77149f9bbf353f0d
Mehar-Aziz <meharaziz.1614>
2024-07-16 14:40:44 +0500

    guess using for loop

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ print(chosen_word)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess the letter: ").lower()
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if guess in chosen_word:
-    print(f"yes, {chosen_word[]}")
-else:
-    print("no")
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
