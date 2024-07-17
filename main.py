1e855f088b96c41e0366fff86a94f23f668e2ea7
Mehar-Aziz <meharaziz.1614>
2024-07-17 14:03:50 +0500

    Debugged

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ print(f'Pssst, the solution is {chosen_word}.')
 lives = 6
 
 display = []
+guess_words = []
 for _ in range(len(chosen_word)):
     display += "_"
 print(display)
@@ -19,14 +20,18 @@ end_of_game = False
 while not end_of_game:
 
     guess = input("Guess a letter: ").lower()
-
-
+    if guess in guess_words:
+        print(f"The letter {guess} is already guessed")
+    else:
+        guess_words.append(guess)
+    
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
 
     if guess not in chosen_word:
+        print(f"The letter {guess} is not in the Word")
         lives -= 1
         if lives == 0:
             end_of_game = True
